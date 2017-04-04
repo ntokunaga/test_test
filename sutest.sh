37ada728ee9a10048b6225294bf829135c360bae
kan-matsumoto <kan-matsumoto@cec-ltd.co.jp>
2017-04-04 10:56:13 +0900

    コマンド接続演算子を修正

--- sutest.sh
+++ sutest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo sutestStart!
-echo `pwd` && sudo su - batchuser && echo `pwd` && echo `ls -l` && mkdir -p $HOME/codebuild_test_dir && echo `ls -l`
+echo `pwd`; sudo su - batchuser; echo `pwd`; echo `ls -l`; mkdir -p $HOME/codebuild_test_dir; echo `ls -l`;
 echo sutestFinish!
 
