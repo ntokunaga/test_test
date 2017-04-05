3a49834ac73d8588ff122fde655ff5269c5e3b48
kan-matsumoto <kan-matsumoto@cec-ltd.co.jp>
2017-04-05 11:44:17 +0900

    シェルを修正

--- sutest.sh
+++ sutest.sh
@@ -5,11 +5,12 @@ devpath = $(pwd)
 echo sutestStart!
 echo `pwd`
 echo pathresult!
-echo ${devpath}
+echo $devpath
+echo $1
 sudo su - batchuser
 echo `pwd`
 echo `ls -l`
-mkdir -p ${devpath}/dev
+mkdir -p $1/dev
 echo `ls -l`
 echo sutestFinish!
 
