50873702110b238cbc10653648c41e86bad192e9
ntokunaga <n_tokunaga@cec-ltd.co.jp>
2017-04-20 15:05:07 +0900

    Update sutest.sh

--- sutest.sh
+++ sutest.sh
@@ -4,5 +4,16 @@
 echo sutestStart!
 echo `yum update -y`
 echo `yum install expect`
+
+useradd UserName 
+expect -c "
+spawn passwd UserName
+expect \"New password:\"
+send -- \"hogehoge\n\"
+expect \"Retype new password:\"
+send -- \"hogehoge\n\"
+expect \"passwd: all authentication tokens updated successfully.\"
+send -- \"exit\n\"
+"
 echo sutestFinish!
 
