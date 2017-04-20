c4c62050b877d0353487e719d7dce2f75dda16d2
ntokunaga <n_tokunaga@cec-ltd.co.jp>
2017-04-20 14:56:20 +0900

    Update sutest.sh

--- sutest.sh
+++ sutest.sh
@@ -2,20 +2,7 @@
 
 
 echo sutestStart!
-echo `su - batchuser; whoami`
-echo `pwd`
-echo pathresult!
-echo $(cd $(dirname $0) && pwd)
-echo `pwd`
-echo `ls -l`
-mkdir -p $(cd $(dirname $0) && pwd)/dev
-echo `ls -l`
-touch $(cd $(dirname $0) && pwd)/test.log
-echo `ls -l`
-sudo chmod 755 $(cd $(dirname $0) && pwd)/test.log
-echo `ls -l`
-aws s3 cp s3://ebisu-bucket/s3test.log $(cd $(dirname $0) && pwd)/
-aws s3 cp s3://ebisu-bucket/s3test.log .
-echo `ls -l`
+echo yum update -y
+echo yum install expect
 echo sutestFinish!
 
