7c752cfe5ac97bc85b921f37b1fb9e205053026a
ntokunaga <n_tokunaga@cec-ltd.co.jp>
2017-04-20 13:41:40 +0900

    Update sutest.sh

--- sutest.sh
+++ sutest.sh
@@ -15,6 +15,7 @@ echo `ls -l`
 sudo chmod 755 $(cd $(dirname $0) && pwd)/test.log
 echo `ls -l`
 aws s3 cp s3://ebisu-bucket/s3test.log $(cd $(dirname $0) && pwd)/
+aws s3 cp s3://ebisu-bucket/s3test.log .
 echo `ls -l`
 echo sutestFinish!
 
